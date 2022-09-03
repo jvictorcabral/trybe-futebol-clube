312e936394d5477cbe45ccf1c68d1820cf5f609d
Joao <joaovictorcgjvcg@gmail.com>
2022-09-03 16:54:08 -0400

    entregando os requisitos 20 e 21

--- app/backend/src/controllers/matchesConstroller.ts
+++ app/backend/src/controllers/matchesConstroller.ts
@@ -2,13 +2,27 @@ import { Request, Response } from 'express';
 import Teams from '../database/models/teams';
 import Matches from '../database/models/matches';
 
-const getMatches = async (_req: Request, res: Response) => {
+const getMatches = async (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+
   const matches = await Matches.findAll({
     include: [
       { model: Teams, as: 'teamHome', attributes: ['teamName'] },
       { model: Teams, as: 'teamAway', attributes: ['teamName'] },
     ],
   });
+
+  if (inProgress === 'true') {
+    const filterInProgress = matches.filter((match) => match.inProgress === true);
+
+    return res.status(200).json(filterInProgress);
+  }
+
+  if (inProgress === 'true') {
+    const filterFinished = matches.filter((match) => match.inProgress !== true);
+
+    return res.status(200).json(filterFinished);
+  }
 
   return res.status(200).json(matches);
 };
