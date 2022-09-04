0be2ee7b74c785c6813a05a3968308d53cd823b8
Joao <joaovictorcgjvcg@gmail.com>
2022-09-04 00:55:45 -0400

    fix: req 19

--- app/backend/src/controllers/matchesConstroller.ts
+++ app/backend/src/controllers/matchesConstroller.ts
@@ -3,26 +3,28 @@ import Teams from '../database/models/teams';
 import Matches from '../database/models/matches';
 
 const getMatches = async (req: Request, res: Response) => {
-  const { inProgress } = req.query;
+  // const { inProgress } = req.query;
 
-  const matches = await Matches.findAll({
-    include: [
-      { model: Teams, as: 'teamHome', attributes: ['teamName'] },
-      { model: Teams, as: 'teamAway', attributes: ['teamName'] },
-    ],
-  });
+  const matches = await Matches.findAll(
+    {
+      include: [
+        { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+        { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    },
+  );
 
-  if (inProgress === 'true') {
-    const filterInProgress = matches.filter((match) => match.inProgress === true);
+  // if (inProgress === 'true') {
+  //   const filterInProgress = matches.filter((match) => match.inProgress === true);
 
-    return res.status(200).json(filterInProgress);
-  }
+  //   return res.status(200).json(filterInProgress);
+  // }
 
-  if (inProgress === 'true') {
-    const filterFinished = matches.filter((match) => match.inProgress !== true);
+  // if (inProgress === 'true') {
+  //   const filterFinished = matches.filter((match) => match.inProgress !== true);
 
-    return res.status(200).json(filterFinished);
-  }
+  //   return res.status(200).json(filterFinished);
+  // }
 
   return res.status(200).json(matches);
 };
